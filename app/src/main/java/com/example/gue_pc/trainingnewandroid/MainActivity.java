a375126947e82a70381feedf78c944f28a8feebf
Dwi Prayogo Kurniadi <dwiprayogok@gmail.com>
2018-01-02 16:37:32 +0700

    My Commit Messages

---
+++ app/src/main/java/com/example/gue_pc/trainingnewandroid/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gue_pc.trainingnewandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text21);
+
+
+
+    }
+}
